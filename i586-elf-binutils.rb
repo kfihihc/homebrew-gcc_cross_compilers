3e2898d17acef383a91733b5db4d6f9e01f56bb7
kfihihc <kfihihc@gmail.com>
2013-10-04 22:46:00 +0800

    Update i586-elf-binutils.rb
    instead gcc-4.2 with llvm-gcc-4.2, and delete the apple-gcc42 depends.

--- i586-elf-binutils.rb
+++ i586-elf-binutils.rb
@@ -5,13 +5,11 @@ class I586ElfBinutils < Formula
   url 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.tar.gz'
   sha1 '470c388c97ac8d216de33fa397d7be9f96c3fe04'
 
-  depends_on 'apple-gcc42' => :build
-
   def install
-    ENV['CC'] = '/usr/local/bin/gcc-4.2'
-    ENV['CXX'] = '/usr/local/bin/g++-4.2'
-    ENV['CPP'] = '/usr/local/bin/cpp-4.2'
-    ENV['LD'] = '/usr/local/bin/gcc-4.2'
+    ENV['CC'] = '/usr/bin/gcc'
+    ENV['CXX'] = '/usr/bin/g++'
+    ENV['CPP'] = '/usr/bin/cpp'
+    ENV['LD'] = '/usr/bin/gcc'
 
     mkdir 'build' do
       system '../configure', '--disable-nls', '--target=i586-elf',
